3de1904db9a279f9524c56c8da7af9ef05fede43
jakhongir-av <jakhongir.av@gmail.com>
2022-12-01 12:44:36 +0500

    End of Pizza

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let preferences = [{Thickness: null, Size: null, smth: [], additionals: [],},];
+let preferences = [{Thickness: null, Size: null, smth: [], additionals: [], price: null,},];
 let SZ = null;
 let result = null;
 let OPT = null;
@@ -10,34 +10,48 @@ form.addEventListener("submit", (e) => {
     const sizes = document.getElementsByName("radios");
     const smth = document.getElementsByName("smth");
     const additionals = document.getElementsByName("additionals");
+
+
     const bread = document.querySelector("#preferences__bread");
     const preferences_size = document.querySelector("#preferences__size");
     const preferences_list = document.querySelector("#preferences__list");
     const preferences_add_list = document.querySelector("#preferences_add__list");
 
     preferences[0].Thickness = selectedOptions.value;
+    bread.innerHTML = selectedOptions.value
+
 
     for(item of sizes) {
         if(item.checked) {
             preferences[0].Size = item.value;
+            preferences_size.innerHTML = item.value;
         }
     }
 
     for(item of smth) {
         if(item.checked) {
+            let val = item.value
             preferences[0].smth.push(item.value)
+            let list = document.createElement("li");
+            preferences_list.appendChild(list);
+            list.innerHTML = item.value;
         }
     }
 
     for(item of additionals) {
         if(item.checked) {
             preferences[0].additionals.push(item.value);
+            let list2 = document.createElement("li");
+            preferences_add_list.appendChild(list2);
+            list2.innerHTML = item.value;
+        }else {
+            let list2 = document.createElement("li");
+            preferences_add_list.appendChild(list2);
+            list2.innerHTML = item.value + ":  Qo'shilmaydi";
         }
     }
     console.log(preferences);
-
-
-    
 })
+
 
 

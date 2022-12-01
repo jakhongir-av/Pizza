cde640f71328e631d16b00b74e7f70a9bde8eb10
jakhongir-av <jakhongir.av@gmail.com>
2022-12-01 05:04:37 +0500

    JS

--- js/main.js
+++ js/main.js
@@ -1,2 +1,31 @@
-const av = "";
+let preferences = [{Thickness: null, Size: null, smth: [], additionals: [],},];
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const selectedOptions = document.querySelector("#options");
+    const sizes = document.getElementsByName("radios");
+    const smth = document.getElementsByName("smth");
+    const additionals = document.getElementsByName("additionals");
+
+    preferences[0].Thickness = selectedOptions.value;
+
+    for(item of sizes) {
+        if(item.checked) {
+            preferences[0].Size = item.value;
+        }
+    }
+
+    for(item of smth) {
+        if(item.checked) {
+            preferences[0].smth.push(item.value)
+        }
+    }
+
+    for(item of additionals) {
+        if(item.checked) {
+            preferences[0].additionals.push(item.value);
+        }
+    }
+    console.log(preferences);
+})
 
